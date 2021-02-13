aeef1d500c043dce23bb942fd46dec81698089b5
DJ_Kaosun <djkaosun@gmail.com>
2021-02-13 09:04:10 +0900

    ライブラリおよびテスト プロジェクトの追加

---
+++ LdapPasswdLib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LdapPasswdLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
